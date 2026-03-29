a6c70e5d21ea952abf27a07c9a825328779ffdda
Andrian <a.krilyshyn@gmail.com>
2026-03-29 18:02:40 +0000

    update outputs

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,20 @@
+
 output "ec2_public_ip" {
-  description = "Публічна IPv4 адреса розгорнутого вебсервера"
+  description = "IP-адреса віртуальної машини"
   value       = aws_instance.web_server.public_ip
 }
 
+output "vm_name" {
+  description = "Ім'я віртуальної машини"
+  value       = aws_instance.web_server.tags["Name"]
+}
+
+output "ami_name" {
+  description = "Ім'я образу використаного для створення ВМ"
+  value       = data.aws_ami.ubuntu_2404.name
+}
+
 output "website_url" {
-  description = "Пряме URL-посилання для доступу до вебсайту"
+  description = "Повна адреса (URL) за якою доступний налаштований сервіс"
   value       = "http://${aws_instance.web_server.public_ip}:${var.web_port}"
 }
